4958de168d0d4aa0d69b927fe94198eaaab154bc
TzeSiong <s10204388@connect.np.edu.sg>
2021-01-03 23:00:08 +0800

    2

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('homeContainer').hover(
+        function(){
+            $(this).animate({
+                marginTop: "-=1%",
+            },200);
+        },
+
+        function(){
+            $(this).animate({
+                marginTop: "0%",
+            },200);
+        }
+    )
+})
